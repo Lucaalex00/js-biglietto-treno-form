203218354bcd8d590fb29439a2fe3a3724cc6e6c
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-20 17:38:12 +0100

    Add EventListener Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,37 @@
 - querySelector
 */
 
-//Variabili Chilometri / Età
-let km = prompt(`Quanti km devi percorrere ?`);
-km = Number(km);
-let age = prompt(`Qual'è la tua età ?`);
-age = Number(age);
-
-//Variabile Costo Biglietto
-var ticketPrice = (km * 0.21);
-ticketPrice = Number(ticketPrice)
-//Sconto minorenni e anziani
-if (age < 18) { //SE meno di 18 anni
-    const minorDiscount = ((ticketPrice / 100) * 20);
-    ticketPrice = (ticketPrice - minorDiscount);
-} else if (age > 65) { //SE piu di 65 anni
-    const oldDiscount = ((ticketPrice / 100) * 40);
-    ticketPrice = (ticketPrice - oldDiscount);
-}
-
-console.log(ticketPrice);
-
-
 //Elements from HTML
+const userInfo = document.querySelector('.userinfo_input')
+const userKm = document.querySelector('.km_input');
+const userAge = document.querySelector('.age_input');
 
-document.querySelector('.km_input').innerHTML = km
-document.querySelector('.age_input')
+
+
+document.getElementById('button').addEventListener('click', function () {
+
+    //Variabili / Chilometri / Età
+    let km = Number(userKm.value);
+    console.log(km + ' km');
+    let age = Number(userAge.value);
+    console.log(age + 'enne')
+
+    //Variabile Costo Biglietto
+    var ticketPrice = (km * 0.21);
+    ticketPrice = Number(ticketPrice)
+    //Sconto minorenni e anziani
+    if (age < 18) { //SE meno di 18 anni
+        const minorDiscount = ((ticketPrice / 100) * 20);
+        ticketPrice = (ticketPrice - minorDiscount);
+    } else if (age > 65) { //SE piu di 65 anni
+        const oldDiscount = ((ticketPrice / 100) * 40);
+        ticketPrice = (ticketPrice - oldDiscount);
+    }
+
+    console.log(ticketPrice.toFixed(2) + ' € è il prezzo del biglietto');
+
+
+
+
+
+});
